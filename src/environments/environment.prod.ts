3a9fa4979ecd35459e4984152ce3c9cdd991c179
abhiram <abhiramreddyk18gmail.com>
2025-05-05 19:28:33 +0530

    updated env.prod  file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://e-commerce-backend-4-r69r.onrender.com/'  
+    apiUrl: 'https://e-commerce-backend-4-r69r.onrender.com'  
   };
